c5d4939ff5e449472f110120ee3e95b7217494ef
Yubin <yubinjodev@gmail.com>
2023-06-22 17:25:23 +0900

    search bar added

---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,9 @@
+import { Form } from "react-bootstrap";
+
+export default function Searchbar(){
+    return(
+        <Form>
+            <Form.Control type="text" placeholder="Search"/>
+        </Form>
+    )
+}
