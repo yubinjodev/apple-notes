c4a929b6d7edf4faab00b97484a9f1234672f608
Yubin <yubinjodev@gmail.com>
2023-06-22 17:23:14 +0900

    window controls added

---
+++ src/components/WindowControl.tsx
@@ -0,0 +1,11 @@
+import { Button } from "react-bootstrap";
+
+export default function WindowControl(){
+    return(
+        <div>
+            <Button variant="success">Close</Button>
+            <Button variant="warning">Minimize</Button>
+            <Button variant="danger">Close</Button>
+        </div>
+    )
+}
