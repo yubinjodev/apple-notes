84a007d3f2f6cdb37329b5f2d4ba09d69bfab288
Yubin <yubinjodev@gmail.com>
2023-06-23 18:58:36 +0900

    env variables added

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,70 +1,78 @@
 import { useState } from "react";
 import axios from "axios";
 
-import {useDispatch} from "react-redux"
+import { useDispatch } from "react-redux";
 import { login } from "../actions";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export type SignUpFormProps = {
-  openSignInForm:()=>void;
-  openEditor:()=>void;
-}
+  openSignInForm: () => void;
+  openEditor: () => void;
+};
 
 export type User = {
   email: string | undefined;
   pw: string | undefined;
-}
+};
 
-const URL = "https://api.jsonbin.io/v3/b"
+const URL = "https://api.jsonbin.io/v3/b";
 const config = {
-  headers:{
-    'Content-Type': 'application/json',
-    'X-Master-Key': '$2b$10$r6LNY8lMtQvXzRP/ZfPasuJg9AW.ThYLsXWivowsi5kpi2NF4T2Ka',
-    'X-Access-Key': '$2b$10$.N5NTNbEHsXaUAILlE99Q./3FLOOq2izzW1IFpsKLb0LaX8BtD3oO',
-  }
-}
+  headers: {
+    "Content-Type": process.env.URL,
+    "X-Master-Key": process.env.X_MASTER_KEY,
+    "X-Access-Key":process.env.X_ACCESS_KEY,
+  },
+};
 
 export default function SignUpForm(props: SignUpFormProps) {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const {openSignInForm, openEditor} = props;
+  const { openSignInForm, openEditor } = props;
 
-  const [email, setEmail] = useState<string>()
-  const [pw, setPw] = useState<string>()
-  const [signUpInfo, setSignupInfo] = useState<User>()
+  const [email, setEmail] = useState<string>();
+  const [pw, setPw] = useState<string>();
+  const [signUpInfo, setSignupInfo] = useState<User>();
 
-  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>)=>{
-    setEmail(e.target.value)
-  }
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
 
-  const handleChangePw = (e: React.ChangeEvent<HTMLInputElement>)=>{
-    setPw(e.target.value)
-  }
+  const handleChangePw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPw(e.target.value);
+  };
 
-  const signUp = async () =>{
-    try{
+  const signUp = async () => {
+    try {
       await axios.post(URL, signUpInfo, config);
-    }catch(e){
+    } catch (e) {
       console.log(e);
     }
 
-    alert("Sign Up Successful")
-    dispatch(login())
+    alert("Sign Up Successful");
+    dispatch(login());
     openEditor();
-  }
+  };
 
-  const handleClickSignUp = ()=>{
-    setSignupInfo({email, pw})
-    signUp()
-  }
+  const handleClickSignUp = () => {
+    setSignupInfo({ email, pw });
+    signUp();
+  };
 
   return (
     <Form className="signupform container">
       <Form.Group>
-        <Form.Control type="email" placeholder="Email" onChange={handleChangeEmail}/>
-        <Form.Control type="password" placeholder="Password" onChange={handleChangePw} />
+        <Form.Control
+          type="email"
+          placeholder="Email"
+          onChange={handleChangeEmail}
+        />
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          onChange={handleChangePw}
+        />
 
         <Button variant="primary" type="submit" onClick={handleClickSignUp}>
           Sign Up
